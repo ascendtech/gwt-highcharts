4b0a47f19340ce1343141fbfa5daea87ac0c0a0c
Matthew Davis <matt.davis@ascend-tech.us>
2020-11-27 15:41:50 -0500

    switch to not use plugin

--- highcharts-injector/build.gradle.kts
+++ highcharts-injector/build.gradle.kts
@@ -1,13 +1,4 @@
-plugins {
-    id("us.ascendtech.gwt.lib")
-}
-
-gwt {
-    isIncludeGwtUser = true
-    libs.add("elemento-core")
-}
-
 dependencies {
-    compile(project(":highcharts"))
+    compileOnly("com.google.elemental2:elemental2-dom:1.1.0")
+    compileOnly("com.google.gwt:gwt-user:2.9.0")
 }
-
